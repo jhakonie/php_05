0a3aad6d41a145e47452b838d405aaa9a50cb8a9
Johanna Hakoniemi <jhakonie@c1r6p12.hive.fi>
2020-11-17 22:07:31 +0200

    fixed 08 and 15

--- ex08/ex08.sql
+++ ex08/ex08.sql
@@ -1,3 +1,3 @@
-SELECT last_name, first_name, CAST(birthdate as DATE) FROM user_card 
+SELECT last_name, first_name, DATE(birthdate) AS birthdate FROM user_card
 WHERE birthdate LIKE "%1989%"
 ORDER BY last_name ASC;
